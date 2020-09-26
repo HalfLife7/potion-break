237d5391282b4255cb244657e7a41c91a745d71d
David Nguyen <nguyendavid3322@gmail.com>
2020-09-26 18:03:20 -0400

    heroku fix

--- build/index.js
+++ build/index.js
@@ -139,7 +139,18 @@ app.use(session({
 
   }),
   secret: process.env.SESSION_SECRET
-})); // Initialize Passport!  Also use passport.session() middleware, to support
+}));
+app.use(function (req, res, next) {
+  if (process.env.NODE_ENV === "production") {
+    var reqType = req.headers["x-forwarded-proto"]; // if not https redirect to https unless logging in using OAuth
+
+    if (reqType !== "https") {
+      req.url.indexOf("auth/steam") !== -1 ? next() : res.redirect("https://" + req.headers.host + req.url);
+    }
+  } else {
+    next();
+  }
+}); // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
 
 app.use(passport.initialize());
